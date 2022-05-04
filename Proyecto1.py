3314632f2ed089d1fd10f194a6f37e576d646e3e
Pili Sánchez <sanchezpili6@gmail.com>
2022-05-03 23:35:49 -0500

    normal distribution

--- Proyecto1.py
+++ Proyecto1.py
@@ -6,128 +6,109 @@ from numpy import random
 def generaAleatorios(generador, lista_generador, funcion, lista_funcion, n, bars):
     x0 = lista_generador[0]
     a = lista_generador[1]
+    xn = x0
     no_aleatorios = []
 
     if generador == "mixto":
-        no_aleatorios = mixed_generator(x0, lista_generador[3], lista_generador[1], lista_generador[2], n)
         c = lista_generador[2]
         m = lista_generador[3]
+        m_generador = m
     elif generador == 'multiplicativo':
         c = 0
         m = lista_generador[2]
-        no_aleatorios = multiplicative_generator(x0, m, a, n)
+        m_generador = m - 1
     else:
         return "Por favor elige entre mixto o multiplicativo"
 
+    # GENERA NUMEROS ALEATORIOS MIXTO O MULTIPLICATIVO
+    for i in range(n):
+        xn = (a * (xn) + c) % m
+        no_aleatorio = xn / m_generador
+        no_aleatorios.append(no_aleatorio)
+        #print("Xn : "+ str(xn) + "             No. aleatorio: "+ str(no_aleatorio))
+
     if funcion == 'exponencial':
-        exponential_distribution(lista_funcion)
+        new_random_numbers = exponential_distribution(lista_funcion, no_aleatorios)
     elif funcion == 'normal':
-        normal_distribution(lista_funcion)
+        new_random_numbers = normal_distribution(lista_funcion, no_aleatorios, n)
     elif funcion == 'poisson':
-        poisson_distribution(lista_funcion)
+        new_random_numbers = poisson_distribution(lista_funcion, no_aleatorios)
     elif funcion == 'binomial':
-        binomial_distribution(lista_funcion)
+        new_random_numbers = binomial_distribution(lista_funcion, no_aleatorios)
+    elif funcion == 'uniforme':
+        new_random_numbers = uniform_distribution(lista_funcion, no_aleatorios)
 
-    plt.hist(x=no_aleatorios, bins='auto', color='#0504aa',
-             alpha=0.7, rwidth=0.85)
+    plt.hist(x=new_random_numbers, bins=bars, color='#0504aa', alpha=0.7, rwidth=0.85)
+    #plt.xlim(0,1.8)
     plt.show()
-    return no_aleatorios
 
 
-def mixed_generator(x0, m, a, c, n):
-    no_aleatorios = []
-    xn = x0
-    m_generador = m
-    print("C: " + str(c) + " M: " + str(m))
-    for i in range(n):
-        xn = (a * xn + c) % m
-        no_aleatorio = xn / m_generador
-        no_aleatorios.append(no_aleatorio)
-        print("Xn : " + str(xn) + "             No. aleatorio: " + str(no_aleatorio))
-    return no_aleatorios
+def uniform_distribution(function_list, no_aleatorios):
+    limite_inferior = function_list[0]
+    limite_superior = function_list[1]
+    temp = limite_inferior + (limite_superior - limite_inferior)
+    new_random_numbers = [no_aleatorio * temp for no_aleatorio in no_aleatorios]
+    return new_random_numbers
 
 
-def multiplicative_generator(x0, m, a, n):
-    no_aleatorios = []
-    xn = x0
-    m_generador = m - 1
-    print(" M: " + str(m))
-    for i in range(n):
-        xn = (a * xn) % m
-        no_aleatorio = xn / m_generador
-        no_aleatorios.append(no_aleatorio)
-        print("Xn : " + str(xn) + "             No. aleatorio: " + str(no_aleatorio))
-    return no_aleatorios
-
-
-# plt.hist(x=random.exponential(5, 10000), bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
-# plt.hist(x=multiplicative_generator(5, 7, 3, 1000), bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
-# plt.show()
-
-
-# print(mixed_generator(7, 7, 3, 3, 10000, 0.45))
-
-
-def exponential_distribution(function_list):
-    x = function_list[0]
-    mean = function_list[1]
+def exponential_distribution(function_list, no_aleatorios):
+    mean = 1/function_list[0]
     new_random_numbers = []
-    r = multiplicative_generator(5, 10000, 211, 1000)
-    for number in r:
-        distribution = -1/mean * math.log(number)
+    for number in no_aleatorios:
+        distribution = (-1 / mean * math.log(number))
         new_random_numbers.append(distribution)
     return new_random_numbers
 
 
-def normal_distribution(function_list):
+def normal_distribution(function_list, no_aleatorios, n):
     media = function_list[0]
-    variation = function_list[1]
-    x = function_list[2]
-    exponential = -((x - media) ^ 2) / 2 * variation
-    distribution = (1 / math.sqrt(2 * math.pi * variation)) * math.exp(exponential)
-    return distribution
+    desv = function_list[1]
+    random_numbers = []
+    new_random_numbers = []
+
+    for number in no_aleatorios:
+        exponential = -((number - media) / 2 * desv) ** 2
+        distribution = (1 / (math.sqrt(2 * math.pi) * desv)) * (math.exp(exponential))
+        random_numbers.append(distribution)
+
+    sumatoria = sum(random_numbers)
+    for number in no_aleatorios:
+        for i in range(n):
+            x = media + desv * ((number*i - n / 2) / (math.sqrt(n / 12)))
+            new_random_numbers.append(x)
+
+    return new_random_numbers
 
 
-def poisson_distribution(function_list):
-    x = function_list[0]
-    mean = function_list[1]
-    distribution = ((math.pow(mean, x)) / math.factorial(x)) * math.exp(-mean)
-    return distribution
+def poisson_distribution(function_list, no_aleatorios):
+    mean = function_list[0]
+    new_random_numbers = []
+    for number in no_aleatorios:
+        distribution = (-1 / mean * math.log(number))**-1
+        new_random_numbers.append(distribution)
+    return new_random_numbers
 
 
-def binomial_distribution(function_list):
-    x = function_list[0]
-    p = function_list[1]
-    n = function_list[2]
-    distribution = combination(n, x) * math.pow(p, x) * math.pow((1 - p),(n - x))
-    return distribution
+def binomial_distribution(function_list, no_aleatorios):
+    p = function_list[0]
+    n = function_list[1]
+    new_random_numbers = []
+    for number in no_aleatorios:
+        distribution = (combination(n, number) * math.pow(p, number) * math.pow((1 - p), (n - number)))
+        new_random_numbers.append(distribution)
+    return new_random_numbers
 
 
 def combination(n, r):
-    c = math.factorial(n) / math.factorial(r) * math.factorial(n - r)
+    c = math.factorial(int(n)) / math.factorial(int(r)) * math.factorial(int(n - r))
     return c
 
 
-generador = "mixto"
-lista_generador = [[15, 8, 16, 10], [13, 50, 17, 64], [7, 5, 24, 32], [3, 5, 21, 100], [0, 3, 3, 7]]
-funcion = "poisson"
-lista_funcion = [4, 18]
-n = 10
-bars = 5
-
-#print(generaAleatorios(generador, lista_generador[4], funcion, lista_funcion, n, bars))
-
-generador_m = "multiplicativo"
-lista_generador_m = [[17, 203, 100000], [3, 211, 1000], [7, 5, 64], [1, 6, 13], [2777, 19, 32]]
-funcion_m = "exponencial"
-lista_funcion_m = [3, 18]
-n_m = 10
-bars_m = 50
-
-#print(generaAleatorios(generador_m, lista_generador_m[4], funcion_m, lista_funcion_m, n_m, bars_m))
-
-print(multiplicative_generator(5, 1000, 211, 10))
-#print(mixed_generator(7, 5, 4, 3, 10))
-#print(exponential_distribution([5,2]))
-plt.hist(x=exponential_distribution([5, 2]), bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
-plt.show()
+# Multiplicativo x0, a, m
+# Mixto x0, a, c, m
+# TODOS ESTOS JALAN BIEN
+# generaAleatorios("multiplicativo", [5, 211, 10000], "exponencial", [5], 1000, 5)
+# generaAleatorios("mixto", [7,5,24,32], "uniforme", [3,10], 100, 5)
+#generaAleatorios("multiplicativo", [5, 211, 10000], "poisson", [20], 10000, 5)
+#generaAleatorios("mixto", [5, 211, 16, 1000], "binomial", [0.1, 25], 10000, 20)
+generaAleatorios("mixto", [8,9,13,10000], "normal", [4, 3], 12, 20)
